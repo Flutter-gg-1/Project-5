6a738d045fbd0fc3f4d7713cc1f829f9c892cfe5
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-29 19:38:53 +0300

    Add MyColors

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color primaryColor = Color.fromRGBO(17, 17, 17, 1);
+  static const Color whiteTextColor = Colors.white;
+  static const Color greyTextColor = Colors.grey;
+  static const Color containerBlackColor = Color(0xff1E1E1E);
+  static const Color pinkColor = Color(0xffBDA6F5);
+}
