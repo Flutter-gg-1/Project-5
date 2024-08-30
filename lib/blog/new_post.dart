516eb4b95ed1ede935a54d36b4c4d0312eeafefa
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-30 20:59:01 +0300

    Add Post

--- lib/blog/new_post.dart
+++ lib/blog/new_post.dart
@@ -25,24 +25,67 @@ class NewPost extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Container(
           color: MyColors.primaryColor,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text('Image',
-                  style: TextStyle(color: MyColors.whiteTextColor)),
-              SizedBox(height: context.getHeightScreen(height: 0.01)),
-              MyContainer(
-                height: context.getWidthScreen(width: 0.4),
-                width: context.getWidthScreen(width: 0.9),
-                child:
-                    IconButton(onPressed: () {}, icon: const Icon(Icons.close)),
-              ),
-              SizedBox(height: context.getWidthScreen(width: 0.1)),
-              const Text('Image',
-                  style: TextStyle(color: MyColors.whiteTextColor)),
-              // SizedBox(height: context.getWidthScreen(width: 0.1)),
-              const MyPostTextField(),
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('Image',
+                    style: TextStyle(color: MyColors.whiteTextColor)),
+                SizedBox(height: context.getHeightScreen(height: 0.01)),
+                MyContainer(
+                  height: context.getWidthScreen(width: 0.4),
+                  width: context.getWidthScreen(width: 0.9),
+                  child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.add,
+                        size: 70,
+                      )),
+                ),
+                SizedBox(height: context.getWidthScreen(width: 0.1)),
+                RichText(
+                    text: const TextSpan(children: [
+                  TextSpan(
+                      text: 'Title',
+                      style: TextStyle(color: MyColors.whiteTextColor)),
+                  TextSpan(text: ' *', style: TextStyle(color: Colors.red)),
+                ])),
+                const MyPostTextField(
+                  hintText: 'Enter Your blog title',
+                ),
+                SizedBox(height: context.getWidthScreen(width: 0.1)),
+                RichText(
+                    text: const TextSpan(children: [
+                  TextSpan(
+                      text: 'Content',
+                      style: TextStyle(color: MyColors.whiteTextColor)),
+                  TextSpan(text: ' *', style: TextStyle(color: Colors.red)),
+                ])),
+                SizedBox(height: context.getWidthScreen(width: 0.02)),
+                MyContainer(
+                  height: context.getWidthScreen(width: 0.5),
+                  width: context.getWidthScreen(width: 0.9),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Enter Your blog content',
+                      hintStyle: TextStyle(color: MyColors.greyTextColor),
+                    ),
+                  ),
+                ),
+                SizedBox(height: context.getWidthScreen(width: 0.02)),
+                RichText(
+                    text: const TextSpan(children: [
+                  TextSpan(
+                      text: 'Reading minutes',
+                      style: TextStyle(color: MyColors.whiteTextColor)),
+                  TextSpan(text: ' *', style: TextStyle(color: Colors.red)),
+                ])),
+                const MyPostTextField(
+                  hintText: 'Minutes of reading this blog',
+                ),
+              ],
+            ),
           ),
         ),
       ),
