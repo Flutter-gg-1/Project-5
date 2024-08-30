fdd569fe88ac0843035e0b1a7b149ee204be285b
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-30 19:45:12 +0300

    Add ProfileScreen

--- lib/screen/bottom/profile.dart
+++ lib/screen/bottom/profile.dart
@@ -7,10 +7,40 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: MyColors.whiteTextColor,
-      body: Center(
-        child: Text('Profile'),
+    return Scaffold(
+      backgroundColor: MyColors.primaryColor,
+      body: Column(
+        children: [
+          SizedBox(height: context.getHeightScreen(height: 0.1)),
+          const Align(
+            alignment: Alignment.centerLeft,
+            child: Text('Account',
+                style: TextStyle(color: MyColors.whiteTextColor)),
+          ),
+          SizedBox(height: context.getHeightScreen(height: 0.2)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(Icons.search, color: MyColors.whiteTextColor),
+              SizedBox(width: context.getWidthScreen(width: 0.02)),
+              const Text(
+                'My Blogs',
+                style: TextStyle(color: MyColors.whiteTextColor),
+              ),
+            ],
+          ),
+          SizedBox(height: context.getHeightScreen(height: 0.1)),
+          MaterialButton(
+            color: MyColors.whiteTextColor,
+            minWidth: context.getWidthScreen(width: 0.6),
+            height: context.getHeightScreen(height: 0.05),
+            onPressed: () {},
+            child: const Text(
+              'Logout',
+              style: TextStyle(color: Colors.red),
+            ),
+          ),
+        ],
       ),
     );
   }
