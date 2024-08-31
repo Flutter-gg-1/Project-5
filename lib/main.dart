22fce05b29a6c89130cd544e5d34056d63f2c37d
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-31 12:17:18 +0300

    UPD ALL UI version 9 getIt

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:project_5/screen/log_in.dart';
+import 'servers/setup.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  setup();
   runApp(const MainApp());
 }
 
--- lib/servers/setup.dart
+++ lib/servers/setup.dart
@@ -2,6 +2,6 @@ import 'package:get_it/get_it.dart';
 import 'package:project_5/servers/user_data_servers.dart';
 
 final getIt = GetIt.instance;
-void setup() {
+Future<void> setup() async {
   getIt.registerSingleton<UserDataServers>(UserDataServers());
 }
