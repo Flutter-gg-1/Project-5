f148a9eacf5b8da4724946f48de9b2466431223e
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-31 12:45:55 +0300

    UPD ALL UI version 11 getIt Good 2

--- lib/servers/user_data_servers.dart
+++ lib/servers/user_data_servers.dart
@@ -2,5 +2,17 @@ import 'package:project_5/model/user.dart';
 
 class UserDataServers {
   List<UserModel> usersData = [
+    UserModel(
+        id: 1,
+        name: 'Abdullah',
+        username: 'Abu_Mukhlef',
+        email: 'Abu_Mukhlef@example.com',
+        password: '1234'),
+    UserModel(
+        id: 2,
+        name: 'Mukhlef',
+        username: 'Al-Shammari',
+        email: 'Al-Shammari@example.com',
+        password: '1234'),
   ];
 }
