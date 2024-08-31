9892f61bf93ed65cab7d0b7c871efdb79c0f3c2f
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-31 12:18:31 +0300

    UPD ALL UI version 9 getIt 1

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:project_5/screen/log_in.dart';
 import 'servers/setup.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  setup();
+  await setup();
   runApp(const MainApp());
 }
 
