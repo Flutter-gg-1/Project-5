d05ee542a84aa270124265734abaa29d98503bef
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-31 07:36:52 +0300

    UPD ALL UI version 2

--- lib/core/common/my_container.dart
+++ lib/core/common/my_container.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:project_5/core/all_file.dart';
 
 class MyContainer extends StatelessWidget {
-  const MyContainer({super.key, this.child, this.width, this.height});
+  const MyContainer({super.key, this.child, this.width, this.height, this.color});
   final Widget? child;
   final double? width;
   final double? height;
+  final Color? color;
   final double radius = 8;
   @override
   Widget build(BuildContext context) {
@@ -13,7 +14,7 @@ class MyContainer extends StatelessWidget {
       width: width ?? context.getWidthScreen(width: 0.96),
       height: height ?? context.getHeightScreen(height: 0.495),
       decoration: BoxDecoration(
-        color: MyColors.containerBlackColor,
+        color: color ?? MyColors.containerBlackColor,
         borderRadius: BorderRadius.all(Radius.circular(radius)),
       ),
       child: child,
--- lib/screen/bottom/profile.dart
+++ lib/screen/bottom/profile.dart
@@ -21,24 +21,35 @@ class ProfileScreen extends StatelessWidget {
           MyContainer(
             width: context.getWidthScreen(width: 0.9),
             height: context.getHeightScreen(height: 0.2),
+            color: MyColors.primaryColor,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  width: context.getWidthScreen(width: 0.21),
-                  height: context.getHeightScreen(height: 0.11),
-                  decoration: BoxDecoration(
-                  color: MyColors.whiteTextColor,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: const Row(children: [
-                    CircleAvatar(
-                      radius: 40,
-                      backgroundImage: NetworkImage(
-                        'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+                Row(
+                  children: [
+                    Container(
+                      width: context.getWidthScreen(width: 0.21),
+                      height: context.getHeightScreen(height: 0.11),
+                      decoration: BoxDecoration(
+                        color: MyColors.whiteTextColor,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const CircleAvatar(
+                        radius: 40,
+                        backgroundImage: NetworkImage(
+                          'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+                        ),
                       ),
                     ),
-                  ]),
+                    const Flexible(
+                      child: ListTile(
+                        title: Text('example',
+                            style: TextStyle(color: MyColors.whiteTextColor)),
+                        subtitle: Text('example@example.com',
+                            style: TextStyle(color: MyColors.whiteTextColor)),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
