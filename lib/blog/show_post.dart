dd2a378aa9889a233a51473f9239243870e8812d
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-31 19:10:16 +0300

    UPD ALL UI version 26 ADD pen

--- lib/blog/show_post.dart
+++ lib/blog/show_post.dart
@@ -1,16 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../core/all_file.dart';
 
 class ShowPost extends StatelessWidget {
   const ShowPost({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text('Show Post'),
-        ],
+    return Scaffold(
+      backgroundColor: MyColors.primaryColor,
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: context.getHeightScreen(height: 0.06)),
+            AppBarContainer(
+              color: MyColors.primaryColor,
+              color2: MyColors.primaryColor,
+              icon: IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: const Icon(Icons.arrow_back_ios,
+                      color: MyColors.whiteTextColor)),
+              icon2: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.bookmark_border,
+                      color: MyColors.whiteTextColor)),
+              icon3: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(FontAwesomeIcons.pen,
+                      color: MyColors.whiteTextColor)),
+              child: const Text(''),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/core/common/bar_container.dart
+++ lib/core/common/bar_container.dart
@@ -3,8 +3,23 @@ import 'package:flutter/material.dart';
 import '../all_file.dart';
 
 class AppBarContainer extends StatelessWidget {
-  const AppBarContainer({super.key});
+  const AppBarContainer(
+      {super.key,
+      this.icon,
+      this.icon2,
+      this.icon3,
+      this.color,
+      this.color2,
+      this.child,
+      this.child2});
 
+  final Widget? icon;
+  final Widget? icon2;
+  final Widget? icon3;
+  final Color? color;
+  final Color? color2;
+  final Widget? child;
+  final Widget? child2;
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -14,15 +29,16 @@ class AppBarContainer extends StatelessWidget {
           Container(
             width: double.infinity,
             height: context.getHeightScreen(height: 0.08),
-            color: MyColors.containerBlackColor,
+            color: color ?? MyColors.containerBlackColor,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 IconButton(
                   onPressed: () {},
-                  icon: Icon(Icons.menu,
-                      color: MyColors.whiteTextColor,
-                      size: context.getHeightScreen(height: 0.05)),
+                  icon: icon ??
+                      Icon(Icons.menu,
+                          color: MyColors.whiteTextColor,
+                          size: context.getHeightScreen(height: 0.05)),
                 ),
                 Row(
                   children: [
@@ -30,17 +46,19 @@ class AppBarContainer extends StatelessWidget {
                       onPressed: () {
                         context.showScreen(const ExploreScreen());
                       },
-                      icon: Icon(Icons.search,
-                          color: MyColors.whiteTextColor,
-                          size: context.getHeightScreen(height: 0.05)),
+                      icon: icon2 ??
+                          Icon(Icons.search,
+                              color: MyColors.whiteTextColor,
+                              size: context.getHeightScreen(height: 0.05)),
                     ),
                     IconButton(
                       onPressed: () {
                         context.showScreen(const NewPost());
                       },
-                      icon: Icon(Icons.add,
-                          color: MyColors.whiteTextColor,
-                          size: context.getHeightScreen(height: 0.05)),
+                      icon: icon3 ??
+                          Icon(Icons.add,
+                              color: MyColors.whiteTextColor,
+                              size: context.getHeightScreen(height: 0.05)),
                     ),
                   ],
                 ),
@@ -48,42 +66,44 @@ class AppBarContainer extends StatelessWidget {
             ),
           ),
           Container(
-            color: MyColors.containerBlackColor,
+            color: color2 ?? MyColors.containerBlackColor,
             height: context.getHeightScreen(height: 0.08),
-            child: const TabBar(
-                labelColor: MyColors.whiteTextColor,
-                unselectedLabelColor: MyColors.greyTextColor,
-                indicatorWeight: 3,
-                indicatorColor: Colors.red,
-                tabs: [
-                  Tab(
-                    text: 'Tech',
-                  ),
-                  Tab(
-                    text: 'AI',
-                  ),
-                  Tab(
-                    text: 'Cloud',
-                  ),
-                  Tab(
-                    text: 'Robotics',
-                  ),
-                  Tab(
-                    text: 'IoT',
-                  ),
-                ]),
+            child: child ??
+                const TabBar(
+                    labelColor: MyColors.whiteTextColor,
+                    unselectedLabelColor: MyColors.greyTextColor,
+                    indicatorWeight: 3,
+                    indicatorColor: Colors.red,
+                    tabs: [
+                      Tab(
+                        text: 'Tech',
+                      ),
+                      Tab(
+                        text: 'AI',
+                      ),
+                      Tab(
+                        text: 'Cloud',
+                      ),
+                      Tab(
+                        text: 'Robotics',
+                      ),
+                      Tab(
+                        text: 'IoT',
+                      ),
+                    ]),
           ),
           SizedBox(
             height: context.getHeightScreen(height: 0.02),
-            child: const TabBarView(
-              children: [
-                Text(''),
-                Text(''),
-                Text(''),
-                Text(''),
-                Text(''),
-              ],
-            ),
+            child: child2 ??
+                const TabBarView(
+                  children: [
+                    Text(''),
+                    Text(''),
+                    Text(''),
+                    Text(''),
+                    Text(''),
+                  ],
+                ),
           ),
         ],
       ),
